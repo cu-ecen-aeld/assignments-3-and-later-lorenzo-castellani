82012c387c0a513216fba3159fe0902d1429c9c9
lorenzo-castellani <lorenzo.castellani@pd.infn.it>
2025-01-18 14:46:31 +0100

    completed
    Signed-off-by: lorenzo-castellani <lorenzo.castellani@pd.infn.it>

---
+++ finder-app/finder.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#Author:  Lorenzo Castellani
+# finder.sh <filesdir> <searchstr>
+#
+
+if [ $# != 2 ] || ! [ -d $1 ]; then
+	echo "use finder.sh <filesdir> <searchstr>"
+	exit 1
+fi
+
+result=$( grep -rc $2 $1/* )
+numfile=0
+numfound=0
+for i in $result
+do
+    found=$(echo $i | awk -F':' '{print $2}')
+    numfound=$(( numfound + $found ))
+    numfile=$(($numfile + 1))
+done
+echo "The number of files are $numfile and the number of matching lines are $numfound"
